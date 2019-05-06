cd18c125e39f8d06981b08132b5fcf4e2b552b21
Ian Brennan <ian.brennan@viget.com>
2019-05-06 18:33:28 -0500

    id issue

--- slack-api.js
+++ slack-api.js
@@ -1,9 +1,9 @@
 var axios = require("axios");
 
-async function getUser(id = "U03G2NCNG") {
+async function getUser(id) {
   var url = `https://slack.com/api/users.profile.get?token=${
     process.env.VIGE_TOKEN
-  }=${id}`;
+  }&user=${id}`;
 
   const resp = await axios.get(url);
   return resp.data;
