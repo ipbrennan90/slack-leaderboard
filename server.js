7ece6ef1821a4e807d755fefb0cdd588ae698c1a
Ian Brennan <ian.brennan@viget.com>
2019-05-06 20:37:30 -0500

    post test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", cache(500), async (_, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.response_url);
+  console.log(req.body);
   resp = {
     response_type: "in_channel",
     text: "Getting your leaderboard ready, get pump'd"
